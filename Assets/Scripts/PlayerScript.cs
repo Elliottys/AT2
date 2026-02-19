5c30fb76d219c2dd44d5ed09531b509223281e88
Elliottys <146836796+Elliottys@users.noreply.github.com>
2026-02-19 12:39:53 +0000

    ver1

--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -1,16 +1,57 @@
+using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerScript : MonoBehaviour
 {
-    public bool canJump = true;
-    public float yVelocity = 0.0f;
+    public enum PlayerActions { None, Dead, Grounded, Walking, Running, InAir, Jumping}
+    public List<PlayerActions> currentActions;
 
-    private void OnJump()
+    
+    public bool tryingJump = false;
+
+    [SerializeField] private float yVelocity = 0.0f;
+    [SerializeField] private float gravity = 16.0f;
+    [SerializeField] private float jumpHeight = 8.0f;
+
+    public void JumpInput(InputAction.CallbackContext context)
     {
-        if (canJump)
+        if (context.phase == InputActionPhase.Started)
         {
-            canJump = false;
-            yVelocity = 8.0f;
+            tryingJump = true;
+        }
+
+        if (context.phase == InputActionPhase.Canceled)
+        {
+            tryingJump = false;
+        }
+    }
+
+    private void JumpAction()
+    {
+        if (currentActions.Contains(PlayerActions.Jumping))
+        {
+            
+        }
+    }
+    
+    private void InAirAction()
+    {
+        Vector3 pos = transform.position;
+
+        // Replace this with an actual check for being grounded
+        if (transform.position.y < 0)
+        {
+            //currentActions.RemoveAll(PlayerActions.InAir);
+            yVelocity = 0.0f;
+            transform.position = new Vector3(pos.x, 0, pos.z);
+        }
+
+        // should be "if not grounded"
+        if (yVelocity != 0)
+        {
+            yVelocity -= gravity * Time.deltaTime;
+            transform.position = new Vector3(pos.x, pos.y + yVelocity * Time.deltaTime, pos.z);
         }
     }
 
@@ -18,19 +59,23 @@ public class PlayerScript : MonoBehaviour
     {
         Vector3 pos = transform.position;
 
-        if (yVelocity != 0)
-        {
-            yVelocity -= 16.0f * Time.deltaTime;
-            transform.position = new Vector3(pos.x, pos.y + yVelocity * Time.deltaTime, pos.z);
-        }
-        
-		// should be "if on ground"
+        // should be "if grounded"
         if (transform.position.y < 0)
         {
-            canJump = true;
+            tryingJump = true;
             yVelocity = 0.0f;
             transform.position = new Vector3(pos.x, 0, pos.z);
         }
+
+        // should be "if not grounded"
+        if (yVelocity != 0)
+        {
+            yVelocity -= gravity * Time.deltaTime;
+            transform.position = new Vector3(pos.x, pos.y + yVelocity * Time.deltaTime, pos.z);
+        }
+        
     }
 
 }
+
+
