3c0585c04a011e33bf2f90e901b4846d375235f0
Elliottys <146836796+Elliottys@users.noreply.github.com>
2026-03-01 22:19:52 +0000

    a1

--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -5,33 +5,33 @@ public class PlayerScript : MonoBehaviour
 {
     // todo: lerp towards gravityDirection by direction amount that increases/decreases?
 
+    public float cameraOffsetX;
+    public float cameraOffsetY;
+    public float speed;
     public Vector3 moveDirection;
     public Vector3 gravityDirection;
-    public float speed;
     public bool gravityInverted;
 
     private Rigidbody body;
+    private Camera mainCamera;
 
     public void ButtonInput(InputAction.CallbackContext context)
     {
-        if (context.phase == InputActionPhase.Started)
+        if (context.phase == InputActionPhase.Performed)
         {
-            gravityInverted = true;
-        }
-
-        if (context.phase == InputActionPhase.Canceled)
-        {
-            gravityInverted = false;
+            gravityInverted = !gravityInverted;
         }
     }
 
     public void Start()
     {
         body = GetComponent<Rigidbody>();
+        mainCamera = Camera.main;
     }
 
     public void Update()
     {
+        mainCamera.transform.position = new Vector3(transform.position.x, cameraOffsetX, cameraOffsetY);
         body.velocity = moveDirection * speed;
         if (gravityInverted)
         {
